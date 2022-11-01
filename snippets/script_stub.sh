4959eaebaa525d26de460fd58fa57156d188f29f
Rustom Shareef <rustom.shareef@gmail.com>
2022-11-01 16:23:41 -0400

    feat: use getopts for script options

--- snippets/script_stub.sh
+++ snippets/script_stub.sh
@@ -7,19 +7,43 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo 'Usage: ./script.sh arg-one arg-two
-
-This is an awesome bash script to make your life better.
-
-'
+usage() {
+    echo "Usage: $0 -a <arg one> [-b <optional arg two>] [-c]"
     exit
+}
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    usage
 fi
 
 # change directory to script dir
 # cd "$(dirname "$0")"
 
 main() {
+    while getopts "a:b:c" opt
+    do
+        case "${opt}" in
+            a) var_a=$OPTARG;;
+            b) var_b=$OPTARG;;
+            c) var_c=1;;
+            *) usage;;
+        esac
+    done
+
+    if [[ ! ${var_a:-} ]]; then
+        usage
+    fi
+
+    echo "a: $var_a"
+
+    if [[ ${var_b:-} ]]; then
+        echo "b: $var_b"
+    fi
+
+    if [[ ${var_c:-} ]]; then
+        echo "c: $var_c"
+    fi
+
     echo "do awesome stuff"
 }
 
